c1a33980093b67b8caaeb1d7c65dbfba7c16933f
pilou33620 <pilou33620@gmail.com>
2018-10-11 14:41:26 +0200

    first commit

---
+++ multiplexeur/multiplexeur.srcs/sources_1/new/Mux4v1.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 04.10.2018 17:52:09
+-- Design Name: 
+-- Module Name: Mux4v1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Mux4v1 is
+    Port ( );
+end Mux4v1;
+
+architecture Behavioral of Mux4v1 is
+
+begin
+
+
+end Behavioral;
